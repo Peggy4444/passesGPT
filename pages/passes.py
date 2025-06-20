fae587bdc7aea86b0a09f94912b8329bebb2a69b
Pegah <rahimian.pegah@gmail.com>
2025-06-20 15:04:02 +0200

    torch removed

--- pages/passes.py
+++ pages/passes.py
@@ -32,8 +32,8 @@ import numpy as np
 import argparse
 import tiktoken
 import os
-from utils.utils import normalize_text
-from utils.utils import SimplerNet
+#from utils.utils import normalize_text
+#from utils.utils import SimplerNet
 
 
 #from classes.visual import PassVisual_logistic as PassVisual
